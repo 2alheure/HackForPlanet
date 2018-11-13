cc71251b460f4a6d40986afad37a1f40ef398725
florianrambur <flo.rambur@gmail.com>
2018-11-13 11:38:11 +0100

    Layout + css/img

---
+++ application/views/layout.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HackForPlanet</title>
+    <link rel="stylesheet" href="<?= toAbsolute('css/style.css') ?>">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+<body>
+    <nav>
+        <div class="text-center">
+            <img class="logo" src="<?= toAbsolute('img/logo.png') ?>" alt="HackForPlanet">
+        </div>
+        <div class="menu">
+            <img src="<?= toAbsolute('img/profil.png') ?>" alt="Profil">
+            <img src="<?= toAbsolute('img/logout.png') ?>" alt="Se déconnecter">
+        </div>
+    </nav>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
