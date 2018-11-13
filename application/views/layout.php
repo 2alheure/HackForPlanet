0060c9fb6bc3b436ef1d02df874c934bbabed24d
florianrambur <flo.rambur@gmail.com>
2018-11-13 11:42:02 +0100

    Added include inside layout.php and created home.php

--- application/views/layout.php
+++ application/views/layout.php
@@ -5,19 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HackForPlanet</title>
-    <link rel="stylesheet" href="<?= toAbsolute('css/style.css') ?>">
+    <link rel="stylesheet" href="<?= toAbsolute('css/style.css'); ?>">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 </head>
 <body>
     <nav>
         <div class="text-center">
-            <img class="logo" src="<?= toAbsolute('img/logo.png') ?>" alt="HackForPlanet">
+            <img class="logo" src="<?= toAbsolute('img/logo.png'); ?>" alt="HackForPlanet">
         </div>
         <div class="menu">
-            <img src="<?= toAbsolute('img/profil.png') ?>" alt="Profil">
-            <img src="<?= toAbsolute('img/logout.png') ?>" alt="Se déconnecter">
+            <img src="<?= toAbsolute('img/profil.png'); ?>" alt="Profil">
+            <img src="<?= toAbsolute('img/logout.png'); ?>" alt="Se déconnecter">
         </div>
     </nav>
+
+    <?php include($file . '.php'); ?>
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </body>
