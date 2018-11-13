1d9110680fdd5c13b1c836dd46ec38827aeb201c
2alheure <jojo77.juv@live.fr>
2018-11-13 15:31:12 +0100

    Fixes slides + Layout

--- application/views/slides.php
+++ application/views/slides.php
@@ -1,4 +1,4 @@
-<div>
+<div id="slides">
 	<div class="fotorama" data-allowfullscreen="native" data-maxheight="100%" data-maxwidth="100%">
 		<?php 
 			$images = array_diff(scandir(BASEPATH.'../assets/img/slides'), ['.', '..']);
