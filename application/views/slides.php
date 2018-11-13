8599c8c28946f5d8f1741116f688cb33bc599584
2alheure <jojo77.juv@live.fr>
2018-11-13 14:58:17 +0100

    Slides OK.

--- application/views/slides.php
+++ application/views/slides.php
@@ -1,5 +1,5 @@
 <div>
-	<div id="slides" data-maxheight="100%" data-maxwidth="100%">
+	<div class="fotorama" data-allowfullscreen="native" data-maxheight="100%" data-maxwidth="100%">
 		<?php 
 			$images = array_diff(scandir(BASEPATH.'../assets/img/slides'), ['.', '..']);
             foreach ($images as $img) { ?>
